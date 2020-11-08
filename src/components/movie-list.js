b4d9db06da8e43f106c7aef338fd70abe9ee03a6
Eden <gresseden@gmail.com>
2020-11-07 20:12:57 -0500

    normalize

--- src/components/movie-list.js
+++ src/components/movie-list.js
@@ -3,8 +3,9 @@ import styled from '../lib/styled-components.js'
 import Wrapper from './wrapper.js'
 import Movie from './movie.js'
 import store from '../store.js'
+import api from '../api.js'
+import { ADD_MOVIES } from '../actions/index.js'
 // import movies from '../movies.js'
-
 
 const MovieListStyled = styled.section`
   display: grid;
@@ -14,20 +15,41 @@ const MovieListStyled = styled.section`
   gap: 1em;
 `
 
-
-
 class MovieList extends Component {
   state = {
-    movies: store.getState().movieList,
-
+    page: 1,
   }
-
+  getPage = async (page) => {
+    const { results } = await api.moviePage(page)
+    store.dispatch({
+      type: ADD_MOVIES,
+      payload: results
+    })
+  }
+  handleIntersection = (entries) => {
+    console.log(entries)
+    if(entries[0].isIntersecting) {
+      console.log("bring new page")
+      this.getPage(this.state.page)
+      this.setState({
+        page: this.state.page + 1
+      })
+    }
+  }
+  componentDidMount() {
+    // this.getPage(this.state.page)
+    store.subscribe(() => {
+      this.setState()
+    })
+    const observer = new IntersectionObserver(this.handleIntersection)
+    observer.observe(window.intersector)
+    // debugger
+  }
   render() {
     const state = store.getState()
     const movieListId = state.list[state.filter]
     const movieList = state.movieList
     console.log(state)
-    const { movies } = this.state
     return Wrapper({
       children: MovieListStyled({
         children: movieListId.map(id => new Movie(movieList.get(id)))
--- src/normalize.js
+++ src/normalize.js
@@ -1,103 +1,37 @@
-// import rawMoviesList from './movies.js'
-
-function movieListAsMap (newList, movie){
-  return newList.reduce((list, movie) => { 
+function movieListAsMap(newList, oldList = new Map()) {
+  return newList.reduce((list, movie) => {
     list.set(movie.id, movie)
     return list
-  }, new Map())
-} 
-
-
-function getAllIds(list){
-  return list.map(movie => movie.id)
+  }, oldList)
 }
-// console.log(movieList) 
 
-function getLeastValuedIds (list) {
+
+function getAllIds(list, oldList = []) {
+  return oldList.concat(list.map(movie => movie.id))
+}
+
+function getMostValuedIds(list, oldList = []) {
   return list.reduce((list, movie) => {
-    if(movie.vote_average <= 7){
+    if (movie.vote_average >= 7) {
       list.push(movie.id)
     }
     return list
-  }, [])
+  }, oldList)
 }
 
-/* 
-Teniendo esta funcion en cuenta, usando el metofdo reduce:
-list.reduce((list, movie) => {
-  if(movie.vote_average > 7.5){
-    list.push(movie)
-  }
-}, []) -> donde [] es el parametro list
- 
-list es el array []
-*/
-
-// const all = rawMoviesList.map((movie) => {
-//   return movie.id
-// })
-
-// // console.log(all)
-
-function getMostValuedIds(list){
+function getLeastValuedIds(list, oldList = []) {
   return list.reduce((list, movie) => {
-    if(movie.vote_average > 7){
+    if (movie.vote_average < 7) {
       list.push(movie.id)
     }
     return list
-  }, [])
-
-
-
+  }, oldList)
 }
 
-
-// console.log(popular)
-
-// const notPopular = rawMoviesList.reduce((list, movie) => {
-//   if(movie.vote_average <= 7){
-//     list.push(movie.id)
-//   }
-//   return list
-// }, [])
-
-// console.log(notPopular)
-
-
-/* 
-sacar elementos anidados de un array a un array plano
-funciona quitandole los curly brackets
-
-const anidado = [1, 2, [3, 4], 5, 6, [7, 8]]
-
-const flatArray = anidado.reduce((acc, el) => 
-  acc.concat(el), [])
-
-console.log(flatArray)
-
-const mascotas = [
-  {nombre: 'eden',edad: 56 ,genero: 'male'},
-  {nombre: 'alfredo',edad:23 ,genero: 'male'},
-  {nombre: 'patricia',edad: 60 ,genero: 'female'},
-  {nombre: 'adriana',edad:31 ,genero: 'female'},
-  {nombre: 'sophia',edad: 6 ,genero: 'female'},
-  {nombre: 'lucia',edad:2 ,genero: 'female'},
-]
-
-const indexed = mascotas.reduce((acc, el) => ({
-  ...acc,
-  [el.nombre]: el
-}), {})
-
-console.log(indexed)
-console.log(indexed['patricia'])
-*/ 
 
 export {
   movieListAsMap,
   getAllIds,
   getMostValuedIds,
   getLeastValuedIds,
-  
-  
 }
